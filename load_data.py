cc80ab1ade798ba3c57c1d3663f0c3ee9b210971
larsmarb <larsmartinbg@gmail.com>
2024-11-13 11:31:16 +0100

    create load functions

---
+++ load_data.py
@@ -0,0 +1,11 @@
+import sklearn.datasets
+
+random_state = 2024 #REPLACE WITH YAML VALUE
+
+#import toy dataset
+def toy_data(n_samples: int, random_state: int = random_state):
+    pass
+
+#import CMU face dataset
+def face_data():
+    pass
