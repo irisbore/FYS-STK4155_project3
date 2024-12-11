ef94e96c0015ffc1fbea0cba73066df0afea2986
irisbore <irisbore@gmail.com>
2024-12-11 17:28:48 +0100

    continued on bootstrap

--- src/evaluation/CNN/run_cnn_eval.py
+++ src/evaluation/CNN/run_cnn_eval.py
@@ -6,29 +6,35 @@ import torchvision
 import git
 import sys
 
-from src.utils import utils
 PATH_TO_ROOT = git.Repo(".", search_parent_directories=True).working_dir
 sys.path.append(PATH_TO_ROOT)
+from src.utils import utils, model_utils
+from src.models.CNN import ConvNet
 
-from models.CNN import ConvNet
+def get_bootstrap_sample(dataset: torch.Tensor, config: dict, seed: int) -> DataLoader:
+        batch_size = config["batch_size"]
+        N = len(dataset)
+        torch.manual_seed(i)
+        rng = np.random.default_rng(seed=i)
+        idx = rng.choice(N, N, replace=True)
+        dataloader = DataLoader(dataset, batch_size=batch_size, sampler=torch.utils.data.SubsetRandomSampler(idx))
+        return dataloader
 
-def bootstrap(dataset: torch.Tensor, config: dict):
+def bootstrap_test_set(testset, model):
+        total_accuracies = []
         for i in tqdm(range(config["n_bootstraps"])):
-                # Generating random indices for sampling from dataframe
-                batch_size = config["batch_size"]
-                N = len(dataset)
-                torch.manual_seed(i)
-                rng = np.random.default_rng(seed=i)
-                idx = rng.choice(N, N, replace=True)
-                dataloader = DataLoader(dataset, batch_size=batch_size, sampler=torch.utils.data.SubsetRandomSampler(idx))
+                testloader = get_bootstrap_sample(testset, config, i)
+                accuracy = model_utils.test_model(testloader, model)
+                total_accuracies.append(accuracy)
+
+        return total_accuracies
 
 
 
-
-
+        
 if __name__=="__main__":
         config_path = utils.get_config_path(
-        default_path=PATH_TO_ROOT + "/src/grid_search/CNN/grid_search.yaml"
+        default_path=PATH_TO_ROOT + "/src/grid_search/CNN/run_cnn_eval"
         )
         config = utils.get_config(config_path)
         torch.manual_seed(config["seed"])
@@ -39,13 +45,21 @@ if __name__=="__main__":
         torchvision.transforms.ToTensor()
         ])
         trainset = torchvision.datasets.MNIST(root=PATH_TO_ROOT+'data/', train=True, download=False, transform=transform)
-        testset = torchvision.datasets.MNIST(root=PATH_TO_ROOT+'data', train=False,transform=transform, download=False) 
+        testset = torchvision.datasets.MNIST(root=PATH_TO_ROOT+'data', train=False,download=False, transform=transform) 
 
         # Model uncertainty
         bootstrap(trainset, config)
+
+        model = model_utils.train_model(trainset)
+
+        PATH = PATH_TO_ROOT+'/saved_models/mnist_net.pth'
+        torch.save(model.state_dict(), PATH)
+
+        net = ConvNet()
+        net.load_state_dict(torch.load(PATH, weights_only=True))
 
         # Train final model
         #model_pipeline()
 
         # Model evaluation
-        #score(testset)
+        total_accuracies = bootstrap_test_set()
---
+++ src/utils/model_utils.py
@@ -0,0 +1,71 @@
+import sys
+from tqdm import tqdm
+from sklearn.model_selection import StratifiedKFold
+import torch
+import torch.nn as nn
+import torch.optim 
+from torch.utils.data import DataLoader
+import git 
+import yaml
+
+PATH_TO_ROOT = git.Repo(".", search_parent_directories=True).working_dir
+sys.path.append(PATH_TO_ROOT)
+
+from src.models.CNN import ConvNet
+from src.models.LogisticRegression import LogisticRegression
+from src.utils import utils
+
+def train_model(trainloader: DataLoader, config: dict, layer_configs=None, learning_rate=None, epochs=None):
+        if learning_rate == None:
+            learning_rate=config['learning_rate']
+
+        if epochs==None:
+            epochs = config['epochs']
+
+        if config['model_type'] == "cnn":
+            model = ConvNet(layer_configs)
+            criterion = nn.CrossEntropyLoss()
+            optimizer  = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        
+        if config['model_type'] == "logreg":
+            model = LogisticRegression()
+            criterion = nn.NLLLoss()
+            optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+            
+        for epoch in tqdm(range(epochs)):
+            running_loss = 0.0
+            for i, data in enumerate(trainloader):
+                inputs, labels = data #list of [inputs, labels]
+                optimizer.zero_grad()
+
+                #forward
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print stats
+                running_loss += loss.item()
+                if i % config['print_interval'] == config['print_interval']-1: #print every interval
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+
+        return model
+
+
+def test_model(testloader, model):         # Test on data set
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = model(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct // total
+    return accuracy
